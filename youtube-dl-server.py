008c4d2a33b094e6967f1bd4add793a5953fbddc
Duncan <duncan@ztkom.net>
2016-12-30 15:33:42 +0000

    hmm should have worked with linking but didn't had to hard code ip address.

--- youtube-dl-server.py
+++ youtube-dl-server.py
@@ -34,7 +34,7 @@ class RedisQueue(object):
     """Simple Queue with Redis Backend"""
     def __init__(self, name, namespace='queue', **redis_kwargs):
         """The default connection parameters are: host='localhost', port=6379, db=0"""
-        self.__db= redis.Redis(**redis_kwargs)
+        self.__db= redis.Redis(host='172.17.0.7', **redis_kwargs)
         self.key = '%s:%s' %(namespace, name)
 
     def qsize(self):
